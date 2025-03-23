9d17b5a477b8dc512bdca189b570622f6b32ed90
Anvar571 <abdurashidovanvar52@gmail.com>
2025-03-23 13:20:55 +0500

    optimization parse body

--- src/Request.ts
+++ src/Request.ts
@@ -1,20 +1,34 @@
-import { IncomingMessage } from "http";
-import { Socket } from "net";
+import { IncomingMessage } from 'http';
+import { Socket } from 'net';
 
 export class HttpRequest extends IncomingMessage {
   constructor(socket: Socket) {
     super(socket);
   }
 
-  public async body() {
+  public async body(limit: number = 1_000_000) {
     return new Promise((res, rej) => {
+      let completed = false;
+      let received = 0;
       let buffer: Buffer[] = [];
 
-      this.on("data", (chunk) => {
+      this.on('data', (chunk) => {
+        if (completed) return;
+
+        if (received > limit) {
+          completed = true;
+          this.destroy();
+          return rej(new Error('Request body too large'));
+        }
+
         buffer.push(chunk);
       });
 
-      this.on("end", () => {
+      this.on('end', () => {
+        if (completed) return;
+
+        completed = true;
+
         const result = Buffer.concat(buffer);
         res(result.toJSON());
       });
